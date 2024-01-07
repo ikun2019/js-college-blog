6811fdef4f9f13fc2d5b3adfe6898788c4d9c9f4
ikun <0215.miyatto@gmail.com>
2024-01-07 11:03:02 +0900

    linkage to notion

---
+++ pages/blogs/[slug].jsx
@@ -0,0 +1,37 @@
+import { getAllPosts, getSinglePost } from '../../lib/notionAPI';
+import ReactMarkdown from 'react-markdown';
+
+const Post = ({ post }) => {
+	return (
+		<section>
+			<h2>{post.metadata.title}</h2>
+			<div>
+				<ReactMarkdown children={post.markdown.parent}></ReactMarkdown>
+			</div>
+		</section>
+	);
+};
+
+export async function getStaticPaths() {
+	const posts = await getAllPosts();
+	const slugs = posts.map((post) => {
+		return { params: { slug: post.slug } };
+	});
+	return {
+		paths: slugs,
+		fallback: 'blocking',
+	};
+}
+
+export async function getStaticProps(context) {
+	const { slug } = context.params;
+	const post = await getSinglePost(slug);
+	return {
+		props: {
+			post,
+		},
+		revalidate: 600,
+	};
+}
+
+export default Post;
